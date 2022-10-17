d3e32aea5213a011eff8c824c4fd98b26b76c9f7
Milton Figueredo Miles <gratno@gmail.com>
2022-10-17 13:15:24 -0500

    Completando ejecicios clase

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ const createTask = (evento) => {
     const task = document.createElement("li");
     task.classList.add("card");
     input.value = "";
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div> <i class="fas fa-trash-alt trashIcon icon"></i>`; //backticks
-    task.innerHTML = content;
+    console.log(checkComplete);
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`; //backticks
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
     console.log(content);
 };
@@ -27,4 +32,13 @@ console.log(btn);
 })*/
 
 btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i")
+    i.classList.add("far");
+    i.classList.add("fa-check-square");
+    i.classList.add("icon");
+
+    return i;
+};
 
