1f5267f6f40fc49afbc90945cd4429d88f497cc3
Milton Figueredo Miles <gratno@gmail.com>
2022-10-17 14:51:22 -0500

    Completando ejecicios

--- script.js
+++ script.js
@@ -35,10 +35,18 @@ btn.addEventListener("click", createTask);
 
 const checkComplete = () => {
     const i = document.createElement("i")
-    i.classList.add("far");
-    i.classList.add("fa-check-square");
-    i.classList.add("icon");
-
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
     return i;
 };
 
+const completeTask = (event) => {
+    //console.log(event.target);
+    const element = event.target;
+    //element.classList.add("fas");
+    //element.classList.add("completeIcon");
+    //element.classList.remove("far");
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+};
