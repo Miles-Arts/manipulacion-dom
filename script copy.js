0effdfce9a8c09bf9cb7ed58fbbbe5123c595ea7
Milton Figueredo Miles <gratno@gmail.com>
2022-11-07 12:53:03 -0500

    Añadiendo librería -moment-

--- script copy.js
+++ script copy.js
@@ -4,33 +4,30 @@ import deleteIcon from "./components/deleteIcon.js";
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
-    evento.preventDefault();
+    evento.preventDefault()
     const input = document.querySelector("[data-form-input]");
-    const calendar = document.querySelector("[data-form-date]");
-    
     const value = input.value;
-    const date = calendar.value;
-    console.log(moment(date).format("DD/MM/YYYY"));
     const list = document.querySelector("[data-list]");
     const task = document.createElement("li");
     task.classList.add("card");
     input.value = "";
-    // BACKTICKS
-
-
     const taskContent = document.createElement("div");
     taskContent.appendChild(checkComplete());
     const titleTask = document.createElement("span");
     titleTask.classList.add("task");
     titleTask.innerText = value;
     taskContent.appendChild(titleTask);
-    // TASK.INNERHTML     
-
-
+    //taskContent.appendChild(deleteIcon());
+    //task.innerHTML = content;
     task.appendChild(taskContent);
     task.appendChild(deleteIcon());
     list.appendChild(task);
 };
-
+//Arrow functions o Funciones anónimas
+/*btn.addEventListener("click", function (evento) {
+    evento.preventDefault()
+    const input = document.querySelector("[data-form-input]");
+    console.log(input.value);
+})*/
 btn.addEventListener("click", createTask);
 
