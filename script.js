2b53fc07d3194bbfa82ec44e274e776bb185f0e5
Milton Figueredo Miles <gratno@gmail.com>
2022-11-07 13:50:47 -0500

    Añadiendo addTask

--- script.js
+++ script.js
@@ -3,15 +3,27 @@ import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector("[data-form-btn]");
 
+const addTask = (evento) => {
+
+    const list = document.querySelector("[data-list]");
+    const task = createTask(evento);
+    list.appendChild(task);
+
+}
+
+
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
     const calendar = document.querySelector("[data-form-date]");
-    
+
     const value = input.value;
     const date = calendar.value;
-    console.log(moment(date).format("DD/MM/YYYY"));
-    const list = document.querySelector("[data-list]");
+
+    const dateFormat = moment(date).format("DD/MM/YYYY");
+    console.log(dateFormat);
+
+
     const task = document.createElement("li");
     task.classList.add("card");
     input.value = "";
@@ -26,11 +38,19 @@ const createTask = (evento) => {
     taskContent.appendChild(titleTask);
     // TASK.INNERHTML     
 
+    const dateElement = document.createElement("span");
+
+    dateElement.innerHTML = dateFormat;
+
 
     task.appendChild(taskContent);
+    task.appendChild(dateElement);
     task.appendChild(deleteIcon());
-    list.appendChild(task);
+    return task;
+
 };
 
-btn.addEventListener("click", createTask);
+
+//arrow functions o funciones anónimas 
+btn.addEventListener("click", addTask);
 
