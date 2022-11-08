1894b270dcc7d33fb054c618128f5614b709e3b7
Milton Figueredo Miles <gratno@gmail.com>
2022-11-08 15:00:15 -0500

    Estructura fecha

---
+++ components/dateElement.js
@@ -0,0 +1,13 @@
+/*export const dateElement = (date) => {
+  const dateElement = document.createElement("li");
+
+}*/
+
+export default (date) => {
+
+    const dateElement = document.createElement("li");
+    dateElement.classList.add("date");
+    dateElement.innerHTML = date;
+    return dateElement;
+
+}
--- components/readTasks.js
+++ components/readTasks.js
@@ -1,15 +1,17 @@
 import { createTask } from "./addTask.js";
+import { dateElement } from "./dateElement.js";
 
 export const readTasks = () => {
 
-    const list =  document.querySelector("[data-list]");
+    const list = document.querySelector("[data-list]");
     console.log(list);
 
     const tasksList = JSON.parse(localStorage.getItem("tasks")) || [];
+    console.log(dateElement("14/09/2022"))
 
     tasksList.forEach((task) => {
-       
-       list.appendChild(createTask(task));
-        
+        list.appendChild(dateElement(task.dateFormat));
+        list.appendChild(createTask(task));
+
     });
 };
