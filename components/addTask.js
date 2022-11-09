70a2647c26230cb260bda84ef8646d77c8e88da3
Milton Figueredo Miles <gratno@gmail.com>
2022-11-09 13:40:46 -0500

    -Delete Icon-  compleado

--- components/addTask.js
+++ components/addTask.js
@@ -51,13 +51,14 @@ export const createTask = ({ value, dateFormat, complete, id }) => {
 
     const taskContent = document.createElement("div");
 
-    console.log(complete);
 
     const check = checkComplete(id);
 
     if (complete) {
-
-      console.log("completada");  
+ 
+        check.classList.toggle("fas");
+        check.classList.toggle("completeIcon");
+        check.classList.toggle("far");
 
     };
 
@@ -73,7 +74,7 @@ export const createTask = ({ value, dateFormat, complete, id }) => {
 
         task.appendChild(taskContent);
         task.appendChild(dateElement);
-        task.appendChild(deleteIcon());
+        task.appendChild(deleteIcon(id));
     return task;
 
 };
